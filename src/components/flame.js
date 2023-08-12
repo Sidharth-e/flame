e5e920567266f7b1f353c8dc9c2489271aba01ea
salim-poumki <127753285+salim-poumki@users.noreply.github.com>
2023-08-12 23:37:05 +0530

    added animations

--- src/components/flame.js
+++ src/components/flame.js
@@ -1,7 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./flame.css";
+// import flameAudioF from "./assets/flameF.mp3";
+// import flameAudioL from "./assets/flameL.mp3";
+// import flameAudioA from "../assests/music/flameA.mp3";
+// import flameAudioM from "./assets/flameM.mp3";
+// import flameAudioE from "./assets/flameE.mp3";
+import Lottie from "lottie-react";
+import flameanimationF from "../assests/animations/flameanimationF.json";
+import flameanimationL from "../assests/animations/flameanimationL.json";
+import flameanimationA from "../assests/animations/flameanimationA.json";
+import flameanimationM from "../assests/animations/flameanimationM.json";
+import flameanimationE from "../assests/animations/flameanimationE.json";
+
+
+
 
 function NameEntry() {
+  const audioRefs = {
+    F: useRef(null),
+    L: useRef(null),
+    A: useRef(null),
+    M: useRef(null),
+    E: useRef(null),
+  };
   const [name1, setName1] = useState("");
   const [name2, setName2] = useState("");
   const [result, setResult] = useState("");
@@ -11,6 +32,8 @@ function NameEntry() {
   const [name1Error, setName1Error] = useState("");
   const [name2Error, setName2Error] = useState("");
   const [isCalculating, setIsCalculating] = useState(false);
+  const [loading, setLoading] = useState(false); // Add loading state
+
 
   const handleName1Change = (event) => {
     setName1(event.target.value);
@@ -66,7 +89,15 @@ function NameEntry() {
     );
     setName1Striked(name1StrikedChars);
     setName2Striked(name2StrikedChars);
+    const audioRef = audioRefs[result];
+    if (audioRef && audioRef.current) {
+      audioRef.current.play();
+    }
     setIsCalculating(false); // Clear loading indicator
+    setLoading(true);
+    setTimeout(() => {
+        setLoading(false);
+      }, 1300);
   };
 
   const getCompatibilityDescription = (result) => {
@@ -96,36 +127,62 @@ function NameEntry() {
     E: "Enemies",
   };
   return (
-    <div className="container">
-      <input
-        type="text"
-        placeholder="Enter Name 1"
-        value={name1}
-        className="input-field"
-        onChange={handleName1Change}
-      />
-      <span className="error">{name1Error}</span>
-      <input
-        type="text"
-        placeholder="Enter Name 2"
-        value={name2}
-        className="input-field"
-        onChange={handleName2Change}
-      />
-      <span className="error">{name2Error}</span>
-      <button onClick={handlePlayFlameGame} disabled={isCalculating} className="button">
-        {isCalculating ?  <span className="calculating">Calculating...</span>: "FLAMES"}
-      </button>
-      <button className="button" onClick={handleResetGame}>Reset Game</button>
+    <>
+      <div className="container">
+        <input
+          type="text"
+          placeholder="Enter Name 1"
+          value={name1}
+          className="input-field"
+          onChange={handleName1Change}
+        />
+        <span className="error">{name1Error}</span>
+        <input
+          type="text"
+          placeholder="Enter Name 2"
+          value={name2}
+          className="input-field"
+          onChange={handleName2Change}
+        />
+        <span className="error">{name2Error}</span>
+        <button
+          onClick={handlePlayFlameGame}
+          disabled={isCalculating}
+          className="button"
+        >
+          {isCalculating ? (
+            <span className="calculating">Calculating...</span>
+          ) : (
+            "FLAMES"
+          )}
+        </button>
+        <button className="button" onClick={handleResetGame}>
+          Reset Game
+        </button>
 
-      <p className="strike">{name1Striked}</p>
-      <p className="strike">{name2Striked}</p>
-      <p className="result result-{result}">{resultMeanings[result]}</p>
-      <p className={`compatibility-description result-${result}`}>
-        {compatibilityDescription}
-      </p>
-      
-    </div>
+        <p className="strike">{name1Striked}</p>
+        <p className="strike">{name2Striked}</p>
+        <p className="result result-{result}"> {resultMeanings[result]}</p>
+        <p className={`compatibility-description result-${result}`}>
+          {compatibilityDescription}
+        </p>
+        {/* <audio ref={audioRefs.F} src={flameAudioF} />
+      <audio ref={audioRefs.L} src={flameAudioL} /> */}
+        {/* <audio ref={audioRefs.A} src={flameAudioA} /> */}
+        {/* <audio ref={audioRefs.M} src={flameAudioM} />
+      <audio ref={audioRefs.E} src={flameAudioE} /> */}
+      </div>
+      {loading & result==="F"?
+      <div className="celebration"><Lottie style={{height:350}} animationData={flameanimationF} /></div>:""}
+      {loading & result==="L"?
+      <div className="celebration"><Lottie style={{height:350}} animationData={flameanimationL} /></div>:""}
+      {loading & result==="A"?
+      <div className="celebration"><Lottie style={{height:350}} animationData={flameanimationA} /></div>:""}
+      {loading & result==="M"?
+      <div className="celebration"><Lottie style={{height:350}} animationData={flameanimationM} /></div>:""}
+      { loading & result==="E"?
+      <div className="celebration"><Lottie style={{height:350}} animationData={flameanimationE} /></div>:""}
+    </>
   );
 }
 
